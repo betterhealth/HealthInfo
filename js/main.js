14ae55301a855ea3683858462d7bf1d2d13869fa
Maitreyee Joshi <maitreyeejoshi@Maitreyees-MacBook-Pro.local>
2019-03-09 22:07:12 -0800

    trying to make a working form

--- js/main.js
+++ js/main.js
@@ -98,7 +98,7 @@
     var $form = $('form#test-form'),
         url = 'https://script.google.com/macros/s/AKfycbwKCU_-HzqEvAX-kPQTIyWXBM1O96BHCmoIsz0TvU29b-3V08Q/exec'
 
-        
+
     $('#submit-form').on('click', function(e) {
       e.preventDefault();
       var jqxhr = $.ajax({
@@ -107,7 +107,7 @@
         dataType: "json",
         data: $form.serializeObject()
       }).success(
-        // do something
+        alert("success")
       );
     })
 
