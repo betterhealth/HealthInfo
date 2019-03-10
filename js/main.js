7d2f59c42d8f4ceb0191184b25897541d3064264
Maitreyee Joshi <maitreyeejoshi@Maitreyees-MacBook-Pro.local>
2019-03-10 14:17:03 -0700

    trying to make a working form

--- js/main.js
+++ js/main.js
@@ -95,69 +95,7 @@
 
     };
 
-// Variable to hold request
-var request;
 
-// Bind to the submit event of our form
-$("#foo").submit(function(event){
-
-    alert("pressed send")
-
-    // Abort any pending request
-    if (request) {
-        request.abort();
-    }
-    // setup some local variables
-    var $form = $(this);
-
-    // Let's select and cache all the fields
-    var $inputs = $form.find("input, select, button, textarea");
-
-    // Serialize the data in the form
-    var serializedData = $form.serialize();
-
-    // Let's disable the inputs for the duration of the Ajax request.
-    // Note: we disable elements AFTER the form data has been serialized.
-    // Disabled form elements will not be serialized.
-    $inputs.prop("disabled", true);
-
-    // Fire off the request to /form.php
-    request = $.ajax({
-        url: "https://script.google.com/macros/s/AKfycbxpemkhbyOseWYKbWvy12Cb733xDQ2RsFZL6vucZhDctg1JoQ3y/exec",
-        type: "post",
-        data: serializedData
-    });
-
-    // Callback handler that will be called on success
-    request.done(function (response, textStatus, jqXHR){
-        // Log a message to the console
-        alert("success!");
-        console.log("Hooray, it worked!");
-        console.log(response);
-        console.log(textStatus);
-        console.log(jqXHR);
-    });
-
-    // Callback handler that will be called on failure
-    request.fail(function (jqXHR, textStatus, errorThrown){
-        // Log the error to the console
-        alert("failure");
-        console.error(
-            "The following error occurred: "+
-            textStatus, errorThrown
-        );
-    });
-
-    // Callback handler that will be called regardless
-    // if the request failed or succeeded
-    request.always(function () {
-        // Reenable the inputs
-        $inputs.prop("disabled", false);
-    });
-
-    // Prevent default posting of form
-    event.preventDefault();
-});
 
    /* photoswipe
     * ----------------------------------------------------- */
